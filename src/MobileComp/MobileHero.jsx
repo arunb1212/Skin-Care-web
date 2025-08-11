d6cad407b8debbce6ed22bc53bf0f1fb7d3c2a92
arunb1212 <arunbhagat302@gmail.com>
2025-08-11 15:12:17 +0530

    Refactor MobileHero and remove unused asset images
    Updated MobileHero.jsx with improved layout and styling for better visual presentation. Removed multiple unused image and SVG assets from the assets directory to clean up the project.

--- src/MobileComp/MobileHero.jsx
+++ src/MobileComp/MobileHero.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 
 const MobileHero = () => {
   return (
-    <div className='w-[full] overflow-hidden '>
-        <div className='text-[80px] text-[#2D3B36] leading-[60px] font-[900] -ml-[10px]'>
-        <h1>GLOWWWW <br />
+    <div className=  '  relative w-[full] '>
+        <div className=' -ml-[10px] top text-[90px] text-[#2D3B36] text-center w-screen leading-[60px] font-[900] '>
+        <h1 className='leading-[70px]'>GLOWWWW <br />
         NATURALLY</h1>
         </div>
         <div className='px-[30px] text-[18px] font-thin text-left'>
             <p>Transform your skincare routine with premium products that restore, protect, and enhance your natural glow every day.</p>
         </div>
 
-<div className='flex flex-col' >
+<div className='flex w-full flex-col' >
     <img className='object-cover w-screen z-[1]' src="./src/assets/ChatGPT Image Jun 15, 2025, 10_24_47 PM 3.png" alt="" />
-    <div>
-        <h1 className='text-[75px] -mt-[40px] -ml-[20px]  text-[#2D3B36] font-bold'>SKINCARE</h1>
+    <div >
+        <h1 className='text-[105px]  w-full sm:text-5xl  -mt-[60px]  -ml-[20px]  text-[#2D3B36] font-bold'>SKINCARE</h1>
     </div>
-    <img className='-mt-[40px] object-cover w-[screen]' src="./src/assets/skin_care_beauty_cosmetic_packaging_design_agency_mumbai_delhi_bangalore_india 4.png" alt="" />
+    <img className='-mt-[50px] object-cover w-[screen]' src="./src/assets/skin_care_beauty_cosmetic_packaging_design_agency_mumbai_delhi_bangalore_india 4.png" alt="" />
 
 
 </div>
